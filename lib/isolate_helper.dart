88d82fa5024cc2f14a7016053962733d7152684d
Federico Chiaravalli <federico.ch@outlook.it>
2022-12-31 12:34:11 +0100

    Fixing helper functions

--- lib/isolate_helper.dart
+++ lib/isolate_helper.dart
@@ -27,81 +27,32 @@ import 'dart:isolate';
 /// completed in response to another event, either a port message or a timer.
 ///
 /// Returns the `SendPort` expecting the single message.
-SendPort singleCompletePort<R, P>(
-    Completer<R> completer, {
-      FutureOr<R> Function(P message)? callback,
-      Duration? timeout,
-      FutureOr<R> Function()? onTimeout,
-    }) {
-  if (callback == null && timeout == null) {
-    return _singleCallbackPort<Object>((response) {
-      _castComplete<R>(completer, response);
-    });
-  }
-  var responsePort = RawReceivePort();
-  Timer? timer;
-  if (callback == null) {
-    responsePort.handler = (response) {
-      responsePort.close();
-      timer?.cancel();
-      _castComplete<R>(completer, response);
-    };
-  } else {
-    var zone = Zone.current;
-    var action = zone.registerUnaryCallback((response) {
-      try {
-        // Also catch it if callback throws.
-        completer.complete(callback(response as P));
-      } catch (error, stack) {
-        completer.completeError(error, stack);
-      }
-    });
-    responsePort.handler = (response) {
-      responsePort.close();
-      timer?.cancel();
-      zone.runUnary(action, response as P);
-    };
-  }
-  if (timeout != null) {
-    timer = Timer(timeout, () {
-      responsePort.close();
-      if (onTimeout != null) {
-        /// workaround for incomplete generic parameters promotion.
-        /// example is available in 'TimeoutFirst with invalid null' test
-        try {
-          completer.complete(Future.sync(onTimeout));
-        } catch (e, st) {
-          completer.completeError(e, st);
-        }
-      } else {
-        completer
-            .completeError(TimeoutException('Future not completed', timeout));
-      }
-    });
-  }
-  return responsePort.sendPort;
+SendPort singleCompletePort(Completer<dynamic> completer) {
+  return _singleCallbackPort((response) {
+    _castComplete(completer, response);
+  });
 }
 
 /// Helper function for [singleCallbackPort].
 ///
 /// Replace [singleCallbackPort] with this
 /// when removing the deprecated parameters.
-SendPort _singleCallbackPort<P>(void Function(P) callback) {
+SendPort _singleCallbackPort(void Function(dynamic) callback) {
   var responsePort = RawReceivePort();
   var zone = Zone.current;
   callback = zone.registerUnaryCallback(callback);
   responsePort.handler = (response) {
     responsePort.close();
-    zone.runUnary(callback, response as P);
+    zone.runUnary(callback, response);
   };
   return responsePort.sendPort;
 }
 
 // Helper function that casts an object to a type and completes a
 // corresponding completer, or completes with the error if the cast fails.
-void _castComplete<R>(Completer<R> completer, Object? value) {
+void _castComplete(Completer<dynamic> completer, dynamic value) {
   try {
-    completer.complete(value as R);
+    completer.complete(value);
   } catch (error, stack) {
     completer.completeError(error, stack);
   }
